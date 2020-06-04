b9cc4629443ddcdf3354b77cfb1a1287e0eaa16b
xiecheng328 <13136666328@163.com>
2020-06-05 00:43:23 +0800

    transform-runtime

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -17,7 +17,8 @@ module.exports = {
                         ['@babel/preset-env', {
                             "useBuiltIns": "entry"
                         }]
-                    ]
+                    ],
+                    plugins: ["@babel/plugin-transform-runtime"]
                 }
             }
         }]
